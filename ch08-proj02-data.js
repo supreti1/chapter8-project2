cf8f04580bd54f1b59111f26dbbb51ba7dc55d65
supreti1 <supreti1@svsu.edu>
2025-06-19 19:25:00 -0400

    ch08-proj02-data.js

---
+++ ch08-proj02-data.js
@@ -0,0 +1,23 @@
+const content = [
+  {
+    name: "British Museum",
+    description: "The British Museum is a public institution dedicated to human history, art and culture.",
+    image: "images/british.jpg",
+    tags: ["History", "Culture", "Free Entry"],
+    colors: ["#d9534f", "#5bc0de", "#5cb85c"]
+  },
+  {
+    name: "Emirates Stadium",
+    description: "Home of Arsenal Football Club, this stadium is a must-see for sports fans.",
+    image: "images/emirates.jpg",
+    tags: ["Sports", "Modern", "Arsenal"],
+    colors: ["#f0ad4e", "#d9534f", "#292b2c"]
+  },
+  {
+    name: "Royal Albert Hall",
+    description: "One of London’s most treasured architectural landmarks and music venues.",
+    image: "images/albert.jpg",
+    tags: ["Music", "Concerts", "Landmark"],
+    colors: ["#5cb85c", "#0275d8", "#d9534f"]
+  }
+];
